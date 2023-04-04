e84a788d167777bd0c080ee95bda11045a2f5c5b
Barbi Borrello <biborrello@hotmail.com>
2023-04-04 11:54:06 -0300

    Parte B

---
+++ Parte B/Ejercicio12B.cpp
@@ -0,0 +1,31 @@
+/*                           Ejercicio 12: 
+Hacer un programa que calcule el resultado de la siguiente expresión: 1-2+3-4+5-6...n*/
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+int main()
+{
+	int numero, suma, resta;
+
+
+	cout<< "Ingrese el numero de elementos a sumar: ";
+	cin>>numero;
+
+	
+	for (int i = 1; i <= numero; i+=2) {
+		suma += i;
+	}
+
+	for (int i = 0; i <= numero; i += 2) {
+		resta -= i;
+	}
+	
+	cout << "\nLa suma total es: " <<suma + resta<<endl;
+
+	getch();
+	
+	return 0;
+}
---
+++ Parte B/Ejercicio7B.cpp
@@ -0,0 +1,36 @@
+es (18 sloc)  416 Bytes
+ 
+
+/*                      Ejercicio 7:
+A-Escriba un programa que calcule usando un while, el valor de: 1 + 2 + 3 +...+ n. */
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	
+	int num,suma;
+	
+	
+	cout<<"Ingrese un numero: ";
+	cin>>num;
+	
+	
+	int i=1;
+	
+	while(i<=num){
+		
+		suma +=i;
+		i++;
+		
+	}
+	
+	cout<<"\nLa Suma es: "<<suma;
+	
+	
+	
+	getch();
+	
+	return 0;
+}
